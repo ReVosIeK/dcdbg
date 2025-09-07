0e6a6d8045aa7c237a5996562890d1a0b4d1112e
ReVosIeK <43447718+ReVosIeK@users.noreply.github.com>
2025-09-07 13:26:37 +0000

    Language change

--- i18n.js
+++ i18n.js
@@ -171,7 +171,7 @@ function t(key) {
     return translations[currentLang][key] || key;
 }
 
-let currentLang = 'en';
+let currentLang = 'eng';
 
 function setLanguage(lang) {
     if (!translations[lang]) {
